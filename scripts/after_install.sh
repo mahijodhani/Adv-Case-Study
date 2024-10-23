7d26b7ba778d4c73a6be7722a50f5953fce8bd1b
Mahi Jodhani <164493944+mahijodhani@users.noreply.github.com>
2024-10-24 03:07:13 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,1 +1,7 @@
-
+#!/bin/bash
+echo "Starting After Install"
+sudo cp -r /opt/codedeploy-agent/deployment-root/*/deployment-archive/* /var/www/html/
+sudo chmod -R 755 /var/www/html
+sudo chown -R apache:apache /var/www/html
+sudo systemctl restart httpd
+echo "After Install completed"
